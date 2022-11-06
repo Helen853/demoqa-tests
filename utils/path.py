90e4cd94002f28e6d49935224025f61082e85b27
Helen <dudarevalen_1997@mail.ru>
2022-11-06 17:28:27 +0500

    -m

--- utils/path.py
+++ utils/path.py
@@ -1,7 +1,13 @@
 from pathlib import Path
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
 
 import resources
 
 
 def to_resource(relative_path):
     return str(Path(resources.__file__).parent.joinpath(relative_path).absolute())
+
+
+service = Service(executable_path="/path/to/chromedriver")
+driver = webdriver.Chrome(service=service)
