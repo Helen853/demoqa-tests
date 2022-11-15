c522544508d212b2b4b0b7d0b4d55915b32e862f
Helen <dudarevalen_1997@mail.ru>
2022-11-15 17:33:59 +0500

    переметила setup chrome

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,12 @@ import os
 from selene.support.shared import browser
 import pytest
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+from utils import attach
+
 
 @pytest.fixture(scope='function', autouse=True)
 def test_browser_management():
@@ -12,3 +18,27 @@ def test_browser_management():
         os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
     )
     browser.config.timeout = float(os.getenv('selene.timeout', '3'))
+
+
+@pytest.fixture(scope='function')
+def setup_chrome():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser = Browser(Config(driver))
+    yield browser
+
+    attach.add_html(browser)
+    attach.add_screen(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
