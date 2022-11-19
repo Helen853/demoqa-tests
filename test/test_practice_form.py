8175b7db5e252ab0c3a1b21c343720ff7b855d6d
Helen <dudarevalen_1997@mail.ru>
2022-11-19 15:37:12 +0500

    переместила setup chrome

--- test/test_practice_form.py
+++ test/test_practice_form.py
@@ -2,9 +2,29 @@ from test.test_data.users import hellen
 from model.pages.registration_form import *
 from model.controls.date import *
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
+
 
 @allure.title("Successful fill form")
-def test_registration_form(setup_chrome):
+def test_registration_form():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
+
     # GIVEN
     given_opened()
 
@@ -16,7 +36,7 @@ def test_registration_form(setup_chrome):
         set_gender(hellen.gender)
         set_number(hellen.user_number)
         date_picker(hellen.birth_month, hellen.birth_year, hellen.birth_day)
-        #date_birth(hellen.date_of_birth)
+        # date_birth(hellen.date_of_birth)
         upload_picture('qa-2-min.png')
         set_adress(hellen.current_address)
         set_state_dropdown(hellen.state)
@@ -41,3 +61,8 @@ def test_registration_form(setup_chrome):
             ('State and City', f'{hellen.state} {hellen.city}')
         ],
     )
+
+    attach.add_html(browser)
+    attach.add_screen(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
